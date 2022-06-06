d5b5152197e86fa4b94519b14ac4e5db457d9356
Vladislav Bobyrev <33667903+VladislavBobyrev@users.noreply.github.com>
2022-06-06 09:43:36 +0300

    Update HomeView.vue

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -3,23 +3,20 @@
     v-if="isTable"
   />
 
-  <button @click="toggleTable" :class="{'danger': isTable}">{{isTable ?  "Удалить": "Сгенерировать"}}</button>
+  <!-- <button @click="toggleTable" :class="{'danger': isTable}">{{isTable ?  "Удалить": "Сгенерировать"}}</button> -->
+  <button @click="toggleTable" v-else>{{isTable ?  "Удалить": "Сгенерировать"}}</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, getCurrentInstance } from 'vue'
+import { defineComponent, ref } from 'vue'
 import AppTable from '@/components/appTable/AppTable.vue'
 
 export default defineComponent({
   components: {AppTable},
-  setup() {
-    const instance = getCurrentInstance();
-    
+  setup() { 
     const isTable = ref(false)
-    const toggleTable = () => {
-      isTable.value = ! isTable.value
-      instance?.proxy?.$forceUpdate();
-    }
+    const toggleTable = () => isTable.value = !isTable.value
+    
     return{
       isTable,
       toggleTable,
